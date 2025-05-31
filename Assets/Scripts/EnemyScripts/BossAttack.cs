d85add8ff300046a3d4bee7fac8208d50bcc3434
jungwook123 <24sunrin119@sunrint.hs.kr>
2025-05-31 23:22:24 +0900

    Revert "playerNormalCommit"
    This reverts commit 469537ab4cce4f93e1627381bbe83745d28687ca.

--- Assets/Scripts/EnemyScripts/BossAttack.cs
+++ Assets/Scripts/EnemyScripts/BossAttack.cs
@@ -33,7 +33,7 @@ public class BossAttack : MonoBehaviour
 
             GameObject tmpObj = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
             Rigidbody2D rb = tmpObj.GetComponent<Rigidbody2D>();
-            rb.linearVelocity = projectileDirection;
+            rb.velocity = projectileDirection;
 
             Collider2D collider = tmpObj.GetComponent<Collider2D>();
             if (collider != null)
